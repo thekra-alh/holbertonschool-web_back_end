5dc64c5b343a03c74288dc143da97d2f58c1073b
thekra-alh <202222146@yu.sa.com>
2026-02-18 17:47:56 +0000

    s

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for concatenating two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings
+
+    Args:
+        str1: First string
+        str2: Second string
+
+    Returns:
+        Concatenated string
+    """
+    return str1 + str2
