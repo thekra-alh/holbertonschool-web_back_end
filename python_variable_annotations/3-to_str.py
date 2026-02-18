f6e2db05a2825f8c3d52886b6b1661ec89a49e12
thekra-alh <202222146@yu.sa.com>
2026-02-18 17:50:35 +0000

    s

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for converting float to string"""
+
+
+def to_str(n: float) -> str:
+    """Convert a float to its string representation
+
+    Args:
+        n: Float number
+
+    Returns:
+        String representation of n
+    """
+    return str(n)
