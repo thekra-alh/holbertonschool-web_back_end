d193b821e151fe21208101dc55df6843ba303d3b
root <root@ip-10-42-171-86.ec2.internal>
2026-02-18 17:43:14 +0000

    s

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for adding two floats"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floats and return the result
+
+    Args:
+        a: First float number
+        b: Second float number
+
+    Returns:
+        Sum of a and b as a float
+    """
+    return a + b
