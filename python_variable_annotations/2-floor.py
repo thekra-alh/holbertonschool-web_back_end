240ed4a50d344323c6a5ce3954c9cda320232943
thekra-alh <202222146@yu.sa.com>
2026-02-18 17:49:02 +0000

    s

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for floor operation"""
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float
+
+    Args:
+        n: Float number
+
+    Returns:
+        Floor of n as an integer
+    """
+    return math.floor(n)
