fd1789b6c3948716ca21b2096b026ddfbe6b8446
thekra-alh <202222146@yu.sa.com>
2026-04-09 14:22:22 +0000

    s

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,21 +1,13 @@
-#!/usr/bin/node
-
-// Display welcome message
 console.log('Welcome to Holberton School, what is your name?');
-
-// Set encoding
 process.stdin.setEncoding('utf8');
 
-// Read input
-process.stdin.on('data', (data) => {
-  const name = data.trim();
-  console.log(`Your name is: ${name}`);
-  
-  // End the program after showing the name
-  process.exit();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-// This will run when process.exit() is called or when program ends
 process.on('exit', () => {
   console.log('This important software is now closing');
 });
