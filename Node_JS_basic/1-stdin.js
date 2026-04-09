337e75861834f0fb843739d9239132aae9331c15
thekra-alh <202222146@yu.sa.com>
2026-04-09 14:01:34 +0000

    s

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,21 @@
+#!/usr/bin/node
+
+// Display welcome message
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+// Set encoding for stdin to handle input properly
+process.stdin.setEncoding('utf8');
+
+// Read user input
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
+    // Display the name input by the user
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
+// Handle end of input (Ctrl+D or pipe)
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
