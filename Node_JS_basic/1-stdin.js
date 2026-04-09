c5cf47f0de9a2f41c95de2ccfd85d1aac9385168
thekra-alh <202222146@yu.sa.com>
2026-04-09 13:36:45 +0000

    s

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,9 @@
-// 1-stdin.js
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const input = process.stdin.read();
-  if (input !== null) {
-    process.stdout.write(`Your name is: ${input}`);
-  }
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (chunk) => {
+  process.stdout.write(`Your name is: ${chunk}`);
 });
 
 process.stdin.on('end', () => {
