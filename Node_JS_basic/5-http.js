f5eb33d95e4805eb50c25a973c442362b9e2f5dc
thekra-alh <202222146@yu.sa.com>
2026-04-09 13:56:24 +0000

    s

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,26 +1,29 @@
 const http = require('http');
+const url = require('url');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
-  if (req.url === '/') {
+const app = http.createServer(async (req, res) => {
+  const reqUrl = url.parse(req.url).pathname;
+  if (reqUrl === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    countStudents(process.argv[2])
-      .then((result) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(`This is the list of our students\n${result}`);
-      })
-      .catch(() => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Cannot load the database');
-      });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found');
+  } else if (reqUrl === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    const path = process.argv[2];
+    let output = '';
+    const originalLog = console.log;
+    console.log = (msg) => { output += `${msg}\n`; };
+    try {
+      await countStudents(path);
+    } catch (err) {
+      console.log = originalLog;
+      res.end('This is the list of our students\nCannot load the database');
+      return;
+    }
+    console.log = originalLog;
+    output = output.trimEnd();
+    res.end(`This is the list of our students\n${output}`);
   }
 });
-
 app.listen(1245);
-
 module.exports = app;
