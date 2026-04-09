acbbb2103c51cfb9d61f1522365eb4a3f9818e07
thekra-alh <202222146@yu.sa.com>
2026-04-09 13:26:20 +0000

    s

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,5 +1,35 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.trim().split('\n');
+    const students = lines.slice(1).filter((line) => line.trim() !== '');
+    const result = [];
+
+    result.push(`Number of students: ${students.length}`);
+
+    const fields = {};
+    students.forEach((student) => {
+      const [firstname, , , field] = student.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    });
+
+    Object.entries(fields).forEach(([field, names]) => {
+      result.push(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    });
+
+    resolve({ total: students.length, fields, output: result.join('\n') });
+  });
+});
 
 const app = express();
 
@@ -8,12 +38,32 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', (req, res) => {
-  countStudents(process.argv[2])
-    .then((result) => {
-      res.send(`This is the list of our students\n${result}`);
+  const databasePath = process.argv[2];
+  countStudents(databasePath)
+    .then(({ output }) => {
+      res.send(`This is the list of our students\n${output}`);
     })
-    .catch(() => {
-      res.status(500).send('Cannot load the database');
+    .catch((error) => {
+      res.send(`This is the list of our students\n${error.message}`);
+    });
+});
+
+app.get('/students/:major', (req, res) => {
+  const { major } = req.params;
+
+  if (major !== 'CS' && major !== 'SWE') {
+    res.status(500).send('Major parameter must be CS or SWE');
+    return;
+  }
+
+  const databasePath = process.argv[2];
+  countStudents(databasePath)
+    .then(({ fields }) => {
+      const names = fields[major] || [];
+      res.send(`List: ${names.join(', ')}`);
+    })
+    .catch((error) => {
+      res.status(500).send(error.message);
     });
 });
 
